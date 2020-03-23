796839739a7270601806a1945507cb1a40d77454
Mazz <56042291+mazz-seven@users.noreply.github.com>
2020-03-23 20:03:13 +0200

    export locals_without_parens from .formatter.exs (#59)

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,18 @@
 # Used by "mix format"
+locals_without_parens = [
+  # Surface.Component
+  property: 3,
+  property: 2,
+  context: 1,
+  get: 2,
+  set: 2,
+  set: 3,
+  data: 3,
+  data: 2
+]
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
