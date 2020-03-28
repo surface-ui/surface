f5f3fb503ab0a46d73e36015172c885dcd1ae70b
Marlus Saraiva <marlus.saraiva@gmail.com>
2020-03-28 00:10:07 -0300

    Use slots and add docs to LiveRedirect and LivePatch

--- lib/surface/components/live_patch.ex
+++ lib/surface/components/live_patch.ex
@@ -1,12 +1,38 @@
 defmodule Surface.Components.LivePatch do
   @moduledoc """
-  A module providing similar capabilities to the live_patch function
+  Generates a link that will **patch** the current LiveView.
+
+  Provides similar capabilities to Phoenix's built-in `live_patch/2`
+  function.
+
+  When navigating to the current LiveView, `handle_params/3` is
+  immediately invoked to handle the change of params and URL state.
+  Then the new state is pushed to the client, without reloading the
+  whole page. For live redirects to another LiveView, use
+  `<LiveRedirect>` instead.
   """
+
   use Surface.Component
 
-  property(to, :string, required: true)
-  property(replace, :boolean, default: false)
-  property(class, :css_class, default: "")
+  @doc "The required path to link to"
+  property to, :string, required: true
+
+  @doc "The flag to replace the current history or push a new state"
+  property replace, :boolean, default: false
+
+  @doc "The CSS class for the generated `<a>` element"
+  property class, :css_class, default: ""
+
+  @doc """
+  The label for the generated `<a>` alement, if no content (default slot) is provided.
+  """
+  property label, :string
+
+  @doc """
+  The content of the generated `<a>` element. Note: if no content is provided,
+  the value of property `label` is used instead.
+  """
+  slot default
 
   def render(%{replace: replace} = assigns) do
     link_state = if replace, do: "replace", else: "push"
@@ -19,7 +45,7 @@ defmodule Surface.Components.LivePatch do
       href={{ @to }}
       to={{ @to }}
     >
-      {{ @inner_content.() }}
+      <slot>{{ @label }}</slot>
     </a>
     """
   end
--- lib/surface/components/live_redirect.ex
+++ lib/surface/components/live_redirect.ex
@@ -1,12 +1,38 @@
 defmodule Surface.Components.LiveRedirect do
   @moduledoc """
-  A module providing similar capabilities to the live_redirect function
+  Generates a link that will **redirect** to a new LiveView.
+
+  Provides similar capabilities to Phoenix's built-in `live_redirect/2`
+  function.
+
+  The current LiveView will be shut down and a new one will be mounted
+  in its place, without reloading the whole page. This can
+  also be used to remount the same LiveView, in case you want to start
+  fresh. If you want to navigate to the same LiveView without remounting
+  it, use `<LivePatch>` instead.
   """
+
   use Surface.Component
 
-  property(to, :string, required: true)
-  property(replace, :boolean, default: false)
-  property(class, :css_class, default: "")
+  @doc "The required path to link to"
+  property to, :string, required: true
+
+  @doc "The flag to replace the current history or push a new state"
+  property replace, :boolean, default: false
+
+  @doc "The CSS class for the generated `<a>` element"
+  property class, :css_class, default: ""
+
+  @doc """
+  The label for the generated `<a>` alement, if no content (default slot) is provided.
+  """
+  property label, :string
+
+  @doc """
+  The content of the generated `<a>` element. Note: if no content is provided,
+  the value of property `label` is used instead.
+  """
+  slot default
 
   def render(%{replace: replace} = assigns) do
     link_state = if replace, do: "replace", else: "push"
@@ -19,7 +45,7 @@ defmodule Surface.Components.LiveRedirect do
       href={{ @to }}
       to={{ @to }}
     >
-      {{ @inner_content.() }}
+      <slot>{{ @label }}</slot>
     </a>
     """
   end
