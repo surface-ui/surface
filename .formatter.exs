fd12747ebca353ec9b1b294b918ded1f1a37522d
Mathew Gardner <mathewdgardner@gmail.com>
2020-03-31 18:19:04 -0500

    Add missing comma and slot func in .formatter.exs. (#61)

--- .formatter.exs
+++ .formatter.exs
@@ -8,11 +8,13 @@ locals_without_parens = [
   set: 2,
   set: 3,
   data: 3,
-  data: 2
+  data: 2,
+  slot: 1,
+  slot: 2
 ]
 
 [
   locals_without_parens: locals_without_parens,
-  export: [locals_without_parens: locals_without_parens]
+  export: [locals_without_parens: locals_without_parens],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
