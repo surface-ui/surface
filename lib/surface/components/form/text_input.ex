adc3c0ffbcd40d3459199a6aeab95ac2a033fb0b
Zamith <zamith.28@gmail.com>
2020-04-16 11:39:32 +0100

    Use Phoenix.HTML.Form instead

--- lib/surface/components/form/text_input.ex
+++ lib/surface/components/form/text_input.ex
@@ -1,18 +1,23 @@
 defmodule Surface.Components.Form.TextInput do
   use Surface.Component
 
+  import Phoenix.HTML.Form
+
+  property form, :string, required: true
   property field, :string, required: true
   property value, :string, default: ""
   property class, :css_class
 
   def render(assigns) do
     ~H"""
-    <input
-      type="text"
-      class={{ @class }}
-      id={{ @field }}
-      value={{ @value }}
-    />
+    {{
+      text_input(
+        String.to_atom(@form),
+        @field,
+        value: @value,
+        class: @class
+      )
+    }}
     """
   end
 end
