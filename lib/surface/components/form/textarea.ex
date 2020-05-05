1c623157aabd62506626bb5a7aa42068c896fdcf
Alexandru Bagu <alexandrubagu87@gmail.com>
2020-05-05 23:19:54 +0300

    implements textarea

---
+++ lib/surface/components/form/textarea.ex
@@ -0,0 +1,78 @@
+defmodule Surface.Components.Form.TextArea do
+  @moduledoc """
+  Generates a color input.
+
+  Provides a wrapper for Phoenix.HTML.Form's `textarea/3` function.
+
+  All options passed via `opts` will be sent to `textarea/3`, `value` and
+  `class` can be set directly and will override anything in `opts`.
+
+
+  ## Examples
+
+  ```
+  <TextArea form="user" field="summary" cols="5" rows="10" opts={{ [autofocus: "autofocus"] }}>
+  ```
+  """
+
+  use Surface.Component
+
+  import Phoenix.HTML.Form, only: [textarea: 3]
+  import Surface.Components.Form.Utils
+
+  alias Surface.Components.Form
+
+  @doc "An identifier for the form"
+  property form, :form
+
+  @doc "An identifier for the input"
+  property field, :string, required: true
+
+  @doc "Value to pre-populated the input"
+  property value, :string
+
+  @doc "Class or classes to apply to the input"
+  property class, :css_class
+
+  @doc "Specifies the visible number of lines in a text area"
+  property rows, :string
+
+  @doc "Specifies the visible width of a text area"
+  property cols, :string
+
+  @doc "Keyword list with options to be passed down to `textarea/3`"
+  property opts, :keyword, default: []
+
+  @doc "Triggered when the component loses focus"
+  property blur, :event
+
+  @doc "Triggered when the component receives focus"
+  property focus, :event
+
+  @doc "Triggered when the component receives click"
+  property capture_click, :event
+
+  @doc "Triggered when a button on the keyboard is pressed"
+  property keydown, :event
+
+  @doc "Triggered when a button on the keyboard is released"
+  property keyup, :event
+
+  context get form, from: Form, as: :form_context
+
+  def render(assigns) do
+    form = get_form(assigns)
+    props = get_non_nil_props(assigns, [:value, :class])
+    event_opts = get_events_to_opts(assigns)
+
+    ~H"""
+    {{
+      textarea(
+        form,
+        String.to_atom(@field),
+        props ++ @opts ++ event_opts
+      )
+    }}
+    """
+  end
+end
