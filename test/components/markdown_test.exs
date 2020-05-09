ddd033041def71a95aacc30d63e536b359bc8b90
Marlus Saraiva <marlus.saraiva@gmail.com>
2020-05-09 11:07:19 -0300

    Add global config :default_opts to #Markdown

--- test/components/markdown_test.exs
+++ test/components/markdown_test.exs
@@ -27,23 +27,46 @@ defmodule Surface.Components.MarkdownTest do
            """
   end
 
-  test "use the :default_css_class config as the default class" do
-    Application.put_env(:surface, :components, [
-      {Markdown, default_css_class: "content"}
-    ])
+  describe "global configuration" do
+    test "use the :default_class config as the default class" do
+      Application.put_env(:surface, :components, [
+        {Markdown, default_class: "content"}
+      ])
 
-    html =
-      render_live("""
-      <#Markdown>
-        # Head 1
-      </#Markdown>
-      """)
+      html =
+        render_live("""
+        <#Markdown>
+          # Head 1
+        </#Markdown>
+        """)
 
-    Application.put_env(:surface, :components, [])
+      Application.put_env(:surface, :components, [])
 
-    assert html =~ """
-           <div class="content"><h1>Head 1</h1></div>
-           """
+      assert html =~ """
+             <div class="content"><h1>Head 1</h1></div>
+             """
+    end
+
+    test "use the :default_opts config as the default Earmark options" do
+      Application.put_env(:surface, :components, [
+        {Markdown, default_opts: [code_class_prefix: "language-"]}
+      ])
+
+      html =
+        render_live(~S"""
+        <#Markdown>
+          ```elixir
+          var = 1
+          ```
+        </#Markdown>
+        """)
+
+      Application.put_env(:surface, :components, [])
+
+      assert html =~ """
+             <div><pre><code class="elixir language-elixir">var = 1</code></pre></div>
+             """
+    end
   end
 
   test "do not accept runtime expressions" do
@@ -112,9 +135,9 @@ defmodule Surface.Components.MarkdownTest do
              """
     end
 
-    test "override the :default_css_class config" do
+    test "override the :default_class config" do
       Application.put_env(:surface, :components, [
-        {Markdown, default_css_class: "content"}
+        {Markdown, default_class: "content"}
       ])
 
       html =
@@ -149,11 +172,11 @@ defmodule Surface.Components.MarkdownTest do
   end
 
   describe "property :opts" do
-    test "property opts - forward options to Earmark" do
+    test "forward options to Earmark" do
       assigns = %{}
 
       code = ~H"""
-      <#Markdown opts={{ code_class_prefix: "lang_" }}>
+      <#Markdown opts={{ code_class_prefix: "language-" }}>
         ```elixir
         code
         ```
@@ -161,8 +184,43 @@ defmodule Surface.Components.MarkdownTest do
       """
 
       assert render_static(code) =~ """
-             <pre><code class="elixir lang_elixir">code</code></pre>
+             <pre><code class="elixir language-elixir">code</code></pre>
              """
+    end
+
+    test "gets merged with global config :opts (overrides existing keys)" do
+      Application.put_env(:surface, :components, [
+        {Markdown, default_opts: [code_class_prefix: "language-", smartypants: false]}
+      ])
+
+      html =
+        render_live("""
+        <#Markdown>
+          "Elixir"
+        </#Markdown>
+        """)
+
+      assert html =~ """
+             <div><p>&quot;Elixir&quot;</p></div>
+             """
+
+      html =
+        render_live("""
+        <#Markdown opts={{ smartypants: true }}>
+          "Elixir"
+
+          ```elixir
+          code
+          ```
+        </#Markdown>
+        """)
+
+      assert html =~
+               """
+               <div><p>“Elixir”</p><pre><code class="elixir language-elixir">code</code></pre></div>
+               """
+
+      Application.put_env(:surface, :components, [])
     end
   end
 end
