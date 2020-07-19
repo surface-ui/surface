ae46ce5bce84515e94b3c5b037b53957f687f591
Lloyd Ramey <lloyd.ramey@bti360.com>
2020-07-19 11:16:22 -0400

    Add renderer based on Phoenix.LiveView.Renderer

---
+++ lib/surface/renderer.ex
@@ -0,0 +1,70 @@
+defmodule Surface.Renderer do
+  @moduledoc false
+
+  defmacro __before_compile__(env) do
+    render? = Module.defines?(env.module, {:render, 1})
+    root = Path.dirname(env.file)
+    filename = template_filename(env)
+    template = Path.join(root, filename)
+
+    case {render?, File.exists?(template)} do
+      {true, true} ->
+        IO.warn(
+          "ignoring template #{inspect(template)} because the component " <>
+            "#{inspect(env.module)} defines a render/1 function",
+          Macro.Env.stacktrace(env)
+        )
+
+        :ok
+
+      {true, false} ->
+        :ok
+
+      {false, true} ->
+        ast =
+          template
+          |> File.read!()
+          |> Surface.Translator.run(0, env.module, template)
+          |> EEx.compile_string(
+            engine: Phoenix.LiveView.Engine,
+            line: 0,
+            file: template
+          )
+
+        quote do
+          @file unquote(template)
+          @external_resource unquote(template)
+          def render(var!(assigns)) do
+            unquote(ast)
+          end
+        end
+
+      _ ->
+        message = ~s'''
+        render/1 was not implemented for #{inspect(env.module)}.
+
+        Make sure to either explicitly define a render/1 clause with a Surface template:
+
+            def render(assigns) do
+              ~H"""
+              ...
+              """
+            end
+
+        Or create a file at #{inspect(template)} with the Surface template.
+        '''
+
+        IO.warn(message, Macro.Env.stacktrace(env))
+
+        :ok
+    end
+  end
+
+  defp template_filename(env) do
+    env.module
+    |> Module.split()
+    |> List.last()
+    |> Macro.underscore()
+    |> Kernel.<>(".sface")
+  end
+end
