0c0a3a8252c381ef3ab89ea38699e7cc9d6b252d
Lloyd Ramey <lloyd.ramey@bti360.com>
2020-07-21 22:43:57 -0400

    Add some tests for co-located templates

---
+++ test/support/renderer_test_components.ex
@@ -0,0 +1,14 @@
+defmodule Surface.RendererTest.Components do
+  defmodule ComponentWithExternalTemplate do
+    use Surface.Component
+  end
+
+  defmodule LiveComponentWithExternalTemplate do
+    use Surface.LiveComponent
+  end
+
+  defmodule LiveViewWithExternalTemplate do
+    use Surface.LiveView
+  end
+
+end
