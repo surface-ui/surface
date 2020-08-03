9926e8494eeb123a242e14f1167b6ced58b816bf
Lloyd Ramey <lloyd.ramey@bti360.com>
2020-08-03 19:25:35 -0400

    fix error message assertion

--- test/api_test.exs
+++ test/api_test.exs
@@ -181,7 +181,7 @@ defmodule Surface.APITest do
 
     test "validate unknown type options" do
       code = "property label, :string, a: 1"
-      message = ~r/unknown option :a. Available options: \[:required, :default, :values\]/
+      message = ~r/unknown option :a. Available options: \[:required, :default, :values, :reject_nil\]/
 
       assert_raise(CompileError, message, fn ->
         eval(code)
