b9f49eca8e3596ff49fabda3020db698523e2257
Lloyd Ramey <lloyd.ramey@bti360.com>
2020-08-05 23:25:05 -0400

    remove unused methods from Surface module

--- lib/surface.ex
+++ lib/surface.ex
@@ -90,72 +90,6 @@ defmodule Surface do
   end
 
   @doc false
-  def component(module, assigns) do
-    module.render(assigns)
-  end
-
-  def component(module, assigns, []) do
-    module.render(assigns)
-  end
-
-  @doc false
-  def put_default_props(props, mod) do
-    Enum.reduce(mod.__props__(), props, fn %{name: name, opts: opts}, acc ->
-      default = Keyword.get(opts, :default)
-      Map.put_new(acc, name, default)
-    end)
-  end
-
-  @doc false
-  def begin_context(props, current_context, mod) do
-    assigns = put_gets_into_assigns(props, current_context, mod.__context_gets__())
-
-    initialized_context_assigns =
-      with true <- function_exported?(mod, :init_context, 1),
-           {:ok, values} <- mod.init_context(assigns) do
-        Map.new(values)
-      else
-        false ->
-          []
-
-        {:error, message} ->
-          IOHelper.runtime_error(message)
-
-        result ->
-          IOHelper.runtime_error(
-            "unexpected return value from init_context/1. " <>
-              "Expected {:ok, keyword()} | {:error, String.t()}, got: #{inspect(result)}"
-          )
-      end
-
-    {assigns, new_context} =
-      put_sets_into_assigns_and_context(
-        assigns,
-        current_context,
-        initialized_context_assigns,
-        mod.__context_sets__()
-      )
-
-    assigns = Map.put(assigns, :__surface_context__, new_context)
-
-    {assigns, new_context}
-  end
-
-  @doc false
-  def end_context(context, mod) do
-    Enum.reduce(mod.__context_sets__(), context, fn %{name: name, opts: opts}, acc ->
-      to = Keyword.fetch!(opts, :to)
-      context_entry = acc |> Map.get(to, %{}) |> Map.delete(name)
-
-      if context_entry == %{} do
-        Map.delete(acc, to)
-      else
-        Map.put(acc, to, context_entry)
-      end
-    end)
-  end
-
-  @doc false
   def attr_value(attr, value) do
     if String.Chars.impl_for(value) do
       value
@@ -214,23 +148,6 @@ defmodule Surface do
             "Expected {:ok, keyword()} | {:error, String.t()}, got: #{inspect(result)}"
         )
     end
-  end
-
-  @doc false
-  def style(value, show) when is_binary(value) do
-    if show do
-      quot(value)
-    else
-      semicolon = if String.ends_with?(value, ";") || value == "", do: "", else: ";"
-      quot([value, semicolon, "display: none;"])
-    end
-  end
-
-  def style(value, _show) do
-    IOHelper.runtime_error(
-      "invalid value for attribute \"style\". Expected a string " <>
-        "got: #{inspect(value)}"
-    )
   end
 
   @doc false
@@ -334,44 +251,6 @@ defmodule Surface do
   end
 
   @doc false
-  def on_phx_event(phx_event, [event], caller_cid) do
-    on_phx_event(phx_event, event, caller_cid)
-  end
-
-  def on_phx_event(phx_event, [event | opts], caller_cid) do
-    value = Map.new(opts) |> Map.put(:name, event)
-    on_phx_event(phx_event, value, caller_cid)
-  end
-
-  def on_phx_event(phx_event, %{name: name, target: :live_view}, _caller_cid) do
-    [phx_event, "=", quot(name)]
-  end
-
-  def on_phx_event(phx_event, %{name: name, target: target}, _caller_cid) do
-    [phx_event, "=", quot(name), " phx-target=", quot(target)]
-  end
-
-  # Stateless component or a liveview (no caller_id)
-  def on_phx_event(phx_event, event, nil) when is_binary(event) do
-    [phx_event, "=", quot(event)]
-  end
-
-  def on_phx_event(phx_event, event, caller_cid) when is_binary(event) do
-    [phx_event, "=", quot(event), " phx-target=", to_string(caller_cid)]
-  end
-
-  def on_phx_event(_phx_event, nil, _caller_cid) do
-    []
-  end
-
-  def on_phx_event(phx_event, event, _caller_cid) do
-    IOHelper.runtime_error(
-      "invalid value for \":on-#{phx_event}\". " <>
-        "Expected a :string or :event, got: #{inspect(event)}"
-    )
-  end
-
-  @doc false
   def phx_event(_phx_event, value) when is_binary(value) do
     value
   end
@@ -395,45 +274,6 @@ defmodule Surface do
 
   def event_to_opts(nil, _event_name) do
     []
-  end
-
-  defp quot(value) do
-    [{:safe, "\""}, value, {:safe, "\""}]
-  end
-
-  defp put_gets_into_assigns(assigns, context, gets) do
-    Enum.reduce(gets, assigns, fn %{name: name, opts: opts}, acc ->
-      key = Keyword.get(opts, :as, name)
-      from = Keyword.fetch!(opts, :from)
-      # TODO: raise an error if it's required and it hasn't been set
-      value = context[from][name]
-      Map.put_new(acc, key, value)
-    end)
-  end
-
-  defp put_sets_into_assigns_and_context(assigns, context, values, sets) do
-    Enum.reduce(sets, {assigns, context}, fn %{name: name, opts: opts}, {assigns, context} ->
-      to = Keyword.fetch!(opts, :to)
-      scope = Keyword.get(opts, :scope)
-
-      case Map.fetch(values, name) do
-        {:ok, value} ->
-          new_context_entry =
-            context
-            |> Map.get(to, %{})
-            |> Map.put(name, value)
-
-          new_context = Map.put(context, to, new_context_entry)
-
-          new_assigns =
-            if scope == :only_children, do: assigns, else: Map.put(assigns, name, value)
-
-          {new_assigns, new_context}
-
-        :error ->
-          {assigns, context}
-      end
-    end)
   end
 
   defp maybe_add_class(classes, class) do
