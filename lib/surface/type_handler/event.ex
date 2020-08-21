8d0f92e53b2260a7f591f5c3c1faa9fc0688faa6
Marlus Saraiva <marlus.saraiva@gmail.com>
2020-08-21 16:45:32 -0300

    Update event target on expr_to_quoted

--- lib/surface/type_handler/event.ex
+++ lib/surface/type_handler/event.ex
@@ -4,6 +4,16 @@ defmodule Surface.TypeHandler.Event do
   use Surface.TypeHandler
 
   @impl true
+  def expr_to_quoted(type, attribute_name, clauses, opts, meta, original) do
+    quoted_expr = super(type, attribute_name, clauses, opts, meta, original)
+    caller_cid = Surface.AST.Meta.quoted_caller_cid(meta)
+
+    quote generated: true do
+      unquote(__MODULE__).maybe_update_target(unquote(quoted_expr), unquote(caller_cid))
+    end
+  end
+
+  @impl true
   def expr_to_value([nil], []) do
     {:ok, nil}
   end
@@ -18,15 +28,6 @@ defmodule Surface.TypeHandler.Event do
 
   def expr_to_value(clauses, opts) do
     {:error, {clauses, opts}}
-  end
-
-  @impl true
-  def update_prop_expr(value, meta) do
-    caller_cid = Surface.AST.Meta.quoted_caller_cid(meta)
-
-    quote generated: true do
-      unquote(__MODULE__).maybe_update_target(unquote(value), unquote(caller_cid))
-    end
   end
 
   def maybe_update_target(%{target: nil} = event, nil) do
