3576ebb40d2f207b0d30a3922ee14bd276ba9061
Lloyd Ramey <lnr0626@gmail.com>
2020-09-08 23:47:14 -0400

    remove extract from let

--- lib/surface/directive/let.ex
+++ lib/surface/directive/let.ex
@@ -5,26 +5,7 @@ defmodule Surface.Directive.Let do
       type: :bindings
     ]
 
-  def extract({":let", {:attribute_expr, value, expr_meta}, attr_meta}, meta) do
-    %AST.Directive{
-      module: __MODULE__,
-      name: :let,
-      value: directive_value(value, Map.merge(meta, expr_meta)),
-      meta: Map.merge(meta, attr_meta)
-    }
-  end
-
-  def extract(_, _), do: []
-
   def process(%AST.Directive{value: %AST.AttributeExpr{value: value}}, %{let: let} = node) do
     %{node | let: [value | let]}
-  end
-
-  defp directive_value(value, meta) do
-    %AST.AttributeExpr{
-      value: Surface.TypeHandler.expr_to_quoted!(value, ":let", :bindings, meta),
-      original: value,
-      meta: meta
-    }
   end
 end
