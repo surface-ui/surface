92430316e097276de731d5a31e3f661898e193a1
Lloyd Ramey <lnr0626@gmail.com>
2020-09-09 22:53:38 -0400

    Add support for a transform callback

---
+++ test/transform_test.exs
@@ -0,0 +1,180 @@
+defmodule Surface.TransformTest do
+  use ExUnit.Case
+
+  defmodule Span do
+    use Surface.Component
+
+    def render(assigns) do
+      ~H"""
+      <span><slot /></span>
+      """
+    end
+  end
+
+  defmodule DivToSpan do
+    use Surface.Component
+
+    def render(assigns) do
+      ~H"""
+      <div><slot /></div>
+      """
+    end
+
+    def transform(node) do
+      send(self(), {DivToSpan, "transforming node"})
+      %{node | module: Span}
+    end
+  end
+
+  defmodule LiveDivToSpan do
+    use Surface.LiveComponent
+
+    def render(assigns) do
+      ~H"""
+      <div><slot /></div>
+      """
+    end
+
+    def transform(node) do
+      send(self(), {LiveDivToSpan, "transforming node"})
+      %{node | module: Span, type: Surface.Component}
+    end
+  end
+
+  defmodule LiveDivViewToSpan do
+    use Surface.LiveView
+
+    def render(assigns) do
+      ~H"""
+      <div><slot /></div>
+      """
+    end
+
+    def transform(node) do
+      send(self(), {LiveDivViewToSpan, "transforming node"})
+      %{node | module: Span, type: Surface.Component}
+    end
+  end
+
+  defmodule MacroDivToSpan do
+    use Surface.MacroComponent
+
+    def expand(_, _, _) do
+      Surface.Compiler.compile(
+        """
+        <span>This is a test component. Don't do this at home.</span>
+        """,
+        1,
+        __ENV__
+      )
+    end
+
+    def transform(node) do
+      send(self(), {MacroDivToSpan, "transforming node"})
+      node
+    end
+  end
+
+  defmodule ListProp do
+    use Surface.Component
+
+    property prop, :list
+
+    def render(assigns) do
+      ~H"""
+      <span></span>
+      """
+    end
+
+    def transform(node) do
+      send(self(), {ListProp, "transforming node"})
+      node
+    end
+  end
+
+  test "transform is run on compile when defined for Surface.Component" do
+    code = """
+    <DivToSpan>Some content</DivToSpan>
+    """
+
+    [node | _] = Surface.Compiler.compile(code, 1, __ENV__)
+
+    assert_receive {DivToSpan, "transforming node"}
+
+    assert %Surface.AST.Component{
+             module: Span
+           } = node
+  end
+
+  test "transform is run on compile when defined for Surface.LiveComponent" do
+    code = """
+    <LiveDivToSpan>Some content</LiveDivToSpan>
+    """
+
+    [node | _] = Surface.Compiler.compile(code, 1, __ENV__)
+
+    assert_receive {LiveDivToSpan, "transforming node"}
+
+    assert %Surface.AST.Component{
+             module: Span
+           } = node
+  end
+
+  test "transform is run on compile when defined for Surface.LiveView" do
+    code = """
+    <LiveDivViewToSpan>Some content</LiveDivViewToSpan>
+    """
+
+    [node | _] = Surface.Compiler.compile(code, 1, __ENV__)
+
+    assert_receive {LiveDivViewToSpan, "transforming node"}
+
+    assert %Surface.AST.Component{
+             module: Span
+           } = node
+  end
+
+  test "transform is NOT run on compile when defined for Surface.MacroComponent" do
+    code = """
+    <#MacroDivToSpan>Some content</#MacroDivToSpan>
+    """
+
+    [node | _] = Surface.Compiler.compile(code, 1, __ENV__)
+
+    refute_receive {MacroDivToSpan, "transforming node"}
+
+    assert %Surface.AST.Container{} = node
+  end
+
+  test "transform is not run on parse errors" do
+    code = """
+    <DivToSpan>Invalid syntax (missing end tag)
+    """
+
+    assert_raise(
+      Surface.Compiler.ParseError,
+      "nofile:2: expected closing tag for \"DivToSpan\"",
+      fn ->
+        Surface.Compiler.compile(code, 1, __ENV__)
+      end
+    )
+
+    refute_receive {DivToSpan, "transforming node"}
+  end
+
+  test "transform is not run on compile errors" do
+    code = """
+    <ListProp prop="string" />
+    """
+
+    assert_raise(
+      CompileError,
+      "nofile:1: invalid value for property \"prop\". Expected a :list, got: \"string\".",
+      fn ->
+        Surface.Compiler.compile(code, 1, __ENV__)
+      end
+    )
+
+    refute_receive {ListProp, "transforming node"}
+  end
+end
