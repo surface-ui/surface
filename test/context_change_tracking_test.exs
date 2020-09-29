eba8cc85e4679a57c29ab6825dda1946b3393a1b
Marlus Saraiva <marlus.saraiva@gmail.com>
2020-09-28 21:03:41 -0300

    Fix more tests

--- test/context_change_tracking_test.exs
+++ test/context_change_tracking_test.exs
@@ -28,7 +28,7 @@ defmodule Surface.ContextChangeTrackingTest do
     def render(assigns) do
       ~H"""
       <Context get={{ :field }}>
-        <CheckUpdated id="1" dest={{ @test_pid }} content={{ @field }}/>
+        <CheckUpdated id="1" dest={{ @test_pid }} content={{ field }}/>
         <CheckUpdated id="2" dest={{ @test_pid }}/>
         <div><slot/></div>
       </Context>
