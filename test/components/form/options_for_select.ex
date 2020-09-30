cd6002d39ba52eb609c6445e9aa217921bdbf3e1
Miguel Serrano <miguel@combyne.com>
2020-09-30 11:43:22 +0200

    OptionsForSelect tests

---
+++ test/components/form/options_for_select.ex
@@ -0,0 +1,37 @@
+defmodule Surface.Components.Form.OptionsForSelectTest do
+  use ExUnit.Case, async: true
+
+  alias Surface.Components.Form.OptionsForSelect, warn: false
+
+  import ComponentTestHelper
+
+  test "select" do
+    code = """
+    <OptionsForSelect options={{ ["Admin": "admin", "User": "user"] }} />
+    """
+
+    assert render_live(code) =~ """
+           <option value="admin">Admin</option><option value="user">User</option>
+           """
+  end
+
+  test "passing selected value" do
+    code = """
+    <OptionsForSelect options={{ ["Admin": "admin", "User": "user"] }} selected={{ "admin" }} />
+    """
+
+    assert render_live(code) =~ """
+           <option value="admin" selected="selected">Admin</option><option value="user">User</option>
+           """
+  end
+
+  test "passing multiple selected values" do
+    code = """
+    <OptionsForSelect options={{ ["Admin": "admin", "User": "user"] }} selected={{ ["admin", "user"] }} />
+    """
+
+    assert render_live(code) =~ """
+           <option value="admin" selected="selected">Admin</option><option value="user" selected="selected">User</option>
+           """
+  end
+end
