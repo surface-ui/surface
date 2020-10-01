5b6e3bb78fe7f5e023a726a88947256430aacb3f
Miguel Serrano <miguel@combyne.com>
2020-10-01 19:44:44 +0200

    HiddenInputs tests

---
+++ test/components/form/hidden_inputs_for_test.exs
@@ -0,0 +1,41 @@
+defmodule Surface.Components.Form.HiddenInputsTest do
+  use ExUnit.Case, async: true
+
+  alias Surface.Components.Form, warn: false
+  alias Surface.Components.Form.Inputs, warn: false
+  alias Surface.Components.Form.HiddenInputs, warn: false
+
+  import ComponentTestHelper
+
+  test "using generated form received as slot props" do
+    code = """
+    <Form for={{ :parent }} opts={{ csrf_token: "test" }}>
+      <Inputs for={{ :children }} :let={{ :form, as: :f }}>
+        <HiddenInputs for={{ @f }} />
+      </Inputs>
+    </Form>
+    """
+
+    assert render_live(code) =~ """
+           <form action="#" method="post">\
+           <input name="_csrf_token" type="hidden" value="test"/>\
+           </form>
+           """
+  end
+
+  test "using generated form stored in the Form context" do
+    code = """
+    <Form for={{ :parent }} opts={{ csrf_token: "test" }}>
+      <Inputs for={{ :children }}>
+        <HiddenInputs />
+      </Inputs>
+    </Form>
+    """
+
+    assert render_live(code) =~ """
+           <form action="#" method="post">\
+           <input name="_csrf_token" type="hidden" value="test"/>\
+           </form>
+           """
+  end
+end
