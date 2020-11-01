44cc52a196221e50bba83b542ebaaf852b00ddc1
Lloyd Ramey <lnr0626@gmail.com>
2020-11-01 09:32:33 -0500

    add tests for line numbers

--- test/components/for_test.exs
+++ test/components/for_test.exs
@@ -5,6 +5,19 @@ defmodule Surface.Components.ForTest do
 
   import ComponentTestHelper
 
+  defmodule ListProp do
+    use Surface.Component
+
+    prop prop, :list
+
+    def render(assigns) do
+      ~H"""
+      List?: {{ is_list(@prop) }}
+      <span :for={{ v <- @prop }}>value: {{inspect(v)}}</span>
+      """
+    end
+  end
+
   test "iterates over the provided list" do
     code =
       quote do
@@ -20,5 +33,39 @@ defmodule Surface.Components.ForTest do
            <span>bananas</span>\
            <span>oranges</span>
            """
+  end
+
+  test "parser error message contains the correct line" do
+    code =
+      quote do
+        ~H"""
+        <For each={{ fruit <- ["apples", "bananas", "oranges"] }}>
+          <span>The inner content
+        </For>
+        """
+      end
+
+    message = ~S(code:2: expected closing tag for "span")
+
+    assert_raise(Surface.Compiler.ParseError, message, fn ->
+      render_live(code)
+    end)
+  end
+
+  test "compile error message contains the correct line" do
+    code =
+      quote do
+        ~H"""
+        <For each={{ fruit <- ["apples", "bananas", "oranges"] }}>
+          <ListProp prop="some string">The inner content</ListProp>
+        </For>
+        """
+      end
+
+    message = ~S(code:2: invalid value for property "prop". Expected a :list, got: "some string".)
+
+    assert_raise(CompileError, message, fn ->
+      render_live(code)
+    end)
   end
 end
--- test/components/if_test.exs
+++ test/components/if_test.exs
@@ -5,6 +5,19 @@ defmodule Surface.Components.IfTest do
 
   import ComponentTestHelper
 
+  defmodule ListProp do
+    use Surface.Component
+
+    prop prop, :list
+
+    def render(assigns) do
+      ~H"""
+      List?: {{ is_list(@prop) }}
+      <span :for={{ v <- @prop }}>value: {{inspect(v)}}</span>
+      """
+    end
+  end
+
   test "renders inner if condition is truthy" do
     code =
       quote do
@@ -20,5 +33,39 @@ defmodule Surface.Components.IfTest do
            <span>The inner content</span>\
            <span>with multiple tags</span>
            """
+  end
+
+  test "parser error message contains the correct line" do
+    code =
+      quote do
+        ~H"""
+        <If condition={{ true }}>
+          <span>The inner content
+        </If>
+        """
+      end
+
+    message = ~S(code:2: expected closing tag for "span")
+
+    assert_raise(Surface.Compiler.ParseError, message, fn ->
+      render_live(code)
+    end)
+  end
+
+  test "compile error message contains the correct line" do
+    code =
+      quote do
+        ~H"""
+        <If condition={{ true }}>
+          <ListProp prop="some string">The inner content</ListProp>
+        </If>
+        """
+      end
+
+    message = ~S(code:2: invalid value for property "prop". Expected a :list, got: "some string".)
+
+    assert_raise(CompileError, message, fn ->
+      render_live(code)
+    end)
   end
 end
