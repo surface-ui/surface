be62089f6d51535c1080a81d2395d7e8ae51cee5
Miguel Serrano <miguel@combyne.com>
2020-12-08 15:45:13 +0100

    Migrate test/components/form/number_input_test.exs

--- test/components/form/number_input_test.exs
+++ test/components/form/number_input_test.exs
@@ -1,165 +1,163 @@
 defmodule Surface.Components.Form.NumberInputTest do
-  use ExUnit.Case, async: true
+  use Surface.ConnCase, async: true
 
-  import ComponentTestHelper
-  alias Surface.Components.Form.NumberInput, warn: false
+  alias Surface.Components.Form.NumberInput
 
   test "empty input" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="age" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="user_age" name="user[age]" type="number"/>
+    assert html =~ """
+           <input id="user_age" name="user[age]" type="number">
            """
   end
 
   test "setting the value" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="age" value="33" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="user_age" name="user[age]" type="number" value="33"/>
+    assert html =~ """
+           <input id="user_age" name="user[age]" type="number" value="33">
            """
   end
 
   test "setting the class" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="age" class="input" />
         """
       end
 
-    assert render_live(code) =~ ~r/class="input"/
+    assert html =~ ~r/class="input"/
   end
 
   test "setting multiple classes" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="age" class="input primary" />
         """
       end
 
-    assert render_live(code) =~ ~r/class="input primary"/
+    assert html =~ ~r/class="input primary"/
   end
 
   test "passing other options" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="age" opts={{ autofocus: "autofocus" }} />
         """
       end
 
-    assert render_live(code) =~ """
-           <input autofocus="autofocus" id="user_age" name="user[age]" type="number"/>
+    assert html =~ """
+           <input autofocus="autofocus" id="user_age" name="user[age]" type="number">
            """
   end
 
   test "blur event with parent live view as target" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="color" value="33" blur="my_blur" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="user_color" name="user[color]" phx-blur="my_blur" type="number" value="33"/>
+    assert html =~ """
+           <input id="user_color" name="user[color]" phx-blur="my_blur" type="number" value="33">
            """
   end
 
   test "focus event with parent live view as target" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="color" value="33" focus="my_focus" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="user_color" name="user[color]" phx-focus="my_focus" type="number" value="33"/>
+    assert html =~ """
+           <input id="user_color" name="user[color]" phx-focus="my_focus" type="number" value="33">
            """
   end
 
   test "capture click event with parent live view as target" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="color" value="33" capture_click="my_click" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="user_color" name="user[color]" phx-capture-click="my_click" type="number" value="33"/>
+    assert html =~ """
+           <input id="user_color" name="user[color]" phx-capture-click="my_click" type="number" value="33">
            """
   end
 
   test "keydown event with parent live view as target" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="color" value="33" keydown="my_keydown" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="user_color" name="user[color]" phx-keydown="my_keydown" type="number" value="33"/>
+    assert html =~ """
+           <input id="user_color" name="user[color]" phx-keydown="my_keydown" type="number" value="33">
            """
   end
 
   test "keyup event with parent live view as target" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="color" value="33" keyup="my_keyup" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="user_color" name="user[color]" phx-keyup="my_keyup" type="number" value="33"/>
+    assert html =~ """
+           <input id="user_color" name="user[color]" phx-keyup="my_keyup" type="number" value="33">
            """
   end
 
   test "setting id and name through props" do
-    code =
-      quote do
+    html =
+      render_surface do
         ~H"""
         <NumberInput form="user" field="age" id="old" name="old" />
         """
       end
 
-    assert render_live(code) =~ """
-           <input id="old" name="old" type="number"/>
+    assert html =~ """
+           <input id="old" name="old" type="number">
            """
   end
 end
 
 defmodule Surface.Components.Form.NumberInputConfigTest do
-  use ExUnit.Case
+  use Surface.ConnCase
 
-  alias Surface.Components.Form.NumberInput, warn: false
-  import ComponentTestHelper
+  alias Surface.Components.Form.NumberInput
 
   test ":default_class config" do
     using_config NumberInput, default_class: "default_class" do
-      code =
-        quote do
+      html =
+        render_surface do
           ~H"""
           <NumberInput/>
           """
         end
 
-      assert render_live(code) =~ ~r/class="default_class"/
+      assert html =~ ~r/class="default_class"/
     end
   end
 end
