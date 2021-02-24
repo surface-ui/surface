a66936e137198056a8ba577f6525e152452fbfaa
Marlus Saraiva <marlus.saraiva@gmail.com>
2021-02-24 17:05:08 -0300

    Add support for auto-registrable/live-reloadable hooks (#271)

--- lib/surface/type_handler.ex
+++ lib/surface/type_handler.ex
@@ -316,5 +316,6 @@ defmodule Surface.TypeHandler do
   defp handler(:static_list), do: __MODULE__.StaticList
   defp handler(:context_put), do: __MODULE__.ContextPut
   defp handler(:context_get), do: __MODULE__.ContextGet
+  defp handler(:hook), do: __MODULE__.Hook
   defp handler(_), do: __MODULE__.Default
 end
---
+++ test/support/mix/tasks/compile/surface_test/fake_link.hooks.js
@@ -0,0 +1,2 @@
+let FakeLink = {}
+export { FakeLink }
