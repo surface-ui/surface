4c31ad477a7699576453706cf69624a9b99399d6
harmon25 <dougwright1@gmail.com>
2021-03-26 08:40:29 -0400

    add event props

--- lib/surface/components/live_file_input.ex
+++ lib/surface/components/live_file_input.ex
@@ -16,7 +16,7 @@ defmodule Surface.Components.LiveFileInput do
   """
 
   use Surface.Component
-  import Surface.Components.Form.Utils, only: [props_to_attr_opts: 2]
+  import Surface.Components.Form.Utils, only: [props_to_attr_opts: 2, events_to_opts: 1]
 
   @doc "Upload specified via `allow_upload`"
   prop upload, :struct, required: true
@@ -27,9 +27,25 @@ defmodule Surface.Components.LiveFileInput do
   @doc "Keyword list with options to be passed down to `live_file_input/2`"
   prop opts, :keyword, default: []
 
+  @doc "Triggered when the component loses focus"
+  prop blur, :event
+
+  @doc "Triggered when the component receives focus"
+  prop focus, :event
+
+  @doc "Triggered when the component receives click"
+  prop capture_click, :event
+
+  @doc "Triggered when a button on the keyboard is pressed"
+  prop keydown, :event
+
+  @doc "Triggered when a button on the keyboard is released"
+  prop keyup, :event
+
   def render(assigns) do
     attr_opts = props_to_attr_opts(assigns, class: get_config(:default_class))
+    event_opts = events_to_opts(assigns)
 
-    ~H"{{ live_file_input(@upload, attr_opts ++ @opts) }}"
+    ~H"{{ live_file_input(@upload, attr_opts ++ @opts ++ event_opts) }}"
   end
 end
