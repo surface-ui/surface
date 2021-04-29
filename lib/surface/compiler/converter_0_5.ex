e6c981340a897009d251639be230a7136ca56827
Marlus Saraiva <marlus.saraiva@gmail.com>
2021-04-29 18:53:23 -0300

    Consolidate all syntax changes in Converter_0_5

--- lib/surface/compiler/converter_0_5.ex
+++ lib/surface/compiler/converter_0_5.ex
@@ -7,7 +7,26 @@ defmodule Surface.Compiler.Converter_0_5 do
     String.slice(text, 1..-2)
   end
 
-  # TODO: should we also handle "slot" or shoud we force running Converter_0_4 before?
+  def convert(:unquoted_string, value, _state, _opts) do
+    "{#{value}}"
+  end
+
+  def convert(:tag_name, "template", _state, _opts) do
+    "#template"
+  end
+
+  def convert(:tag_name, "slot", _state, _opts) do
+    "#slot"
+  end
+
+  def convert(:tag_name, "If", _state, _opts) do
+    "#if"
+  end
+
+  def convert(:tag_name, "For", _state, _opts) do
+    "#for"
+  end
+
   def convert(:attr_name, ":props", %{tag_name: "#slot"}, _opts) do
     "args"
   end
--- test/compiler/converter_0_5_test.exs
+++ test/compiler/converter_0_5_test.exs
@@ -8,6 +8,23 @@ defmodule Surface.Compiler.Converter_0_5Test do
     Converter.convert(text, converter: Converter_0_5)
   end
 
+  test "don't convert code inside macros" do
+    expected =
+      convert("""
+      <template slot="footer">Footer</template>
+      <#Raw>
+        <template slot="footer">Footer</template>
+      </#Raw>
+      """)
+
+    assert expected == """
+           <#template slot="footer">Footer</#template>
+           <#Raw>
+             <template slot="footer">Footer</template>
+           </#Raw>
+           """
+  end
+
   test "convert {{ }} into { }" do
     expected =
       convert("""
@@ -30,6 +47,105 @@ defmodule Surface.Compiler.Converter_0_5Test do
              1{ @name }2 3{ @name }4
                  5 { @value } 6
            7 </div>
+           """
+  end
+
+  test "convert unquoted string" do
+    expected =
+      convert("""
+      <div disabled=true>
+        1
+      </div>
+      <div
+        tabindex=2>2</div>
+      """)
+
+    assert expected == """
+           <div disabled={true}>
+             1
+           </div>
+           <div
+             tabindex={2}>2</div>
+           """
+  end
+
+  test "convert <template> into <#template>" do
+    expected =
+      convert("""
+      <div>
+        <template slot="footer">
+          Footer
+        </template>
+      </div>
+      """)
+
+    assert expected == """
+           <div>
+             <#template slot="footer">
+               Footer
+             </#template>
+           </div>
+           """
+  end
+
+  test "convert <slot> into <#slot>" do
+    expected =
+      convert("""
+      <div>
+        <slot name="footer">
+          Footer
+        </slot>
+      </div>
+      """)
+
+    assert expected == """
+           <div>
+             <#slot name="footer">
+               Footer
+             </#slot>
+           </div>
+           """
+  end
+
+  test "convert <If> into <#if>" do
+    expected =
+      convert("""
+      <div>
+        <If condition={{ @var }}>
+          1
+          </If>
+        <If   condition={{@var}}>2</If>
+      </div>
+      """)
+
+    assert expected == """
+           <div>
+             <#if condition={ @var }>
+               1
+               </#if>
+             <#if   condition={@var}>2</#if>
+           </div>
+           """
+  end
+
+  test "convert <For> into <#For>" do
+    expected =
+      convert("""
+      <div>
+        <For each={{ _i <- @var }}>
+          1
+          </For>
+        <For   each={{@var}}>2</For>
+      </div>
+      """)
+
+    assert expected == """
+           <div>
+             <#for each={ _i <- @var }>
+               1
+               </#for>
+             <#for   each={@var}>2</#for>
+           </div>
            """
   end
 
