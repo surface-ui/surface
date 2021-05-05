1c9e6496fcd7058ebae67cd0931a355014720578
Marlus Saraiva <marlus.saraiva@gmail.com>
2021-05-05 08:59:19 -0300

    Fix macro component compile-time dependency (#370)

--- lib/surface/compiler.ex
+++ lib/surface/compiler.ex
@@ -454,9 +454,17 @@ defmodule Surface.Compiler do
          :ok <- validate_properties(mod, attributes, directives, meta) do
       expanded = mod.expand(attributes, children, meta)
 
+      compile_dep_expr = %AST.Expr{
+        value:
+          quote generated: true, line: meta.line do
+            require(unquote(mod)).__compile_dep__()
+          end,
+        meta: meta
+      }
+
       {:ok,
        %AST.Container{
-         children: List.wrap(expanded),
+         children: [compile_dep_expr, expanded],
          directives: directives,
          meta: meta
        }}
