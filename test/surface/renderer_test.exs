49635a59b9af603a1da5c564be163bdb11bda9d9
Lloyd Ramey <lnr0626@gmail.com>
2021-06-10 14:35:38 -0400

    add test indicating issue

--- test/surface/renderer_test.exs
+++ test/surface/renderer_test.exs
@@ -32,3 +32,48 @@ defmodule Surface.RendererTest do
     assert html =~ "the rendered content of the live view"
   end
 end
+
+defmodule Surface.RendererSyncTest do
+  use ExUnit.Case
+
+  import ExUnit.CaptureIO
+
+  test "emitted warning is for sface file" do
+    view_code = """
+    defmodule WarningTestComponent do
+      use Surface.Component
+
+      slot default, required: true
+    end
+    """
+
+    output =
+      capture_io(:standard_error, fn ->
+        {{:module, _, _, _}, _} = Code.eval_string(view_code, [], %{__ENV__ | line: 1})
+      end)
+
+    assert output =~ "setting the fallback content on a required slot has no effect"
+    assert extract_line(output) == 2
+    assert extract_file(output) == "test/surface/warning_test_component.sface"
+  end
+
+  defp extract_line(message) do
+    case Regex.run(~r/(?:nofile|.exs|.sface):(\d+)/, message) do
+      [_, line] ->
+        String.to_integer(line)
+
+      _ ->
+        :not_found
+    end
+  end
+
+  defp extract_file(message) do
+    case Regex.run(~r/(nofile|[^\s]+.exs|[^\s]+.sface):(?:\d+)/, message) do
+      [_, file] ->
+        file
+
+      _ ->
+        :not_found
+    end
+  end
+end
