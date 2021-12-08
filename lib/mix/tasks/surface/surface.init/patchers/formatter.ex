35411c363b53226c0da99f65eadca0639531915d
Marlus Saraiva <marlus.saraiva@gmail.com>
2021-12-08 09:03:28 -0300

    Setup formatter plugin if :plugins doesn't exist

--- lib/mix/tasks/surface/surface.init/patchers/formatter.ex
+++ lib/mix/tasks/surface/surface.init/patchers/formatter.ex
@@ -4,6 +4,7 @@ defmodule Mix.Tasks.Surface.Init.Patchers.Formatter do
   # Common patches for `.formatter`
 
   import Mix.Tasks.Surface.Init.ExPatcher
+  alias Mix.Tasks.Surface.Init.ExPatcher
 
   def add_config(code, key, value) do
     code
@@ -29,10 +30,16 @@ defmodule Mix.Tasks.Surface.Init.Patchers.Formatter do
   end
 
   def add_plugin(code, plugin) do
-    code
-    |> parse_string!()
-    |> find_keyword_value(:plugins)
-    |> halt_if(&find_list_item_with_code(&1, plugin), :already_patched)
-    |> append_list_item(plugin)
+    patcher = parse_string!(code)
+
+    case find_keyword_value(patcher, :plugins) do
+      %ExPatcher{node: nil} ->
+        append_keyword(patcher, :plugins, "[#{plugin}]")
+
+      plugins_patcher ->
+        plugins_patcher
+        |> halt_if(&find_list_item_with_code(&1, plugin), :already_patched)
+        |> append_list_item(plugin)
+    end
   end
 end
