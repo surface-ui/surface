af62476934882feeb39640da0e341141e53d7cf9
Guilherme Sehn <guilherme.sehn@therealreal.com>
2022-01-05 22:00:57 -0300

    support nested views

--- test/surface/view_test.exs
+++ test/surface/view_test.exs
@@ -1,17 +1,31 @@
+defmodule MyAppWeb.FooView do
+  use Phoenix.View, root: "test/support/view_test/templates"
+  use Surface.View, root: "test/support/view_test/templates"
+end
+
+defmodule MyAppWeb.Nested.FooView do
+  use Phoenix.View, root: "test/support/view_test/templates"
+  use Surface.View, root: "test/support/view_test/templates"
+end
+
 defmodule Surface.ViewTest do
   use ExUnit.Case, async: true
 
-  defmodule FooView do
-    use Phoenix.View, root: "test/support/view_test/templates"
-    use Surface.View, root: "test/support/view_test/templates"
-  end
-
   test "generates render/2 function for sface files found on templates folder" do
     result =
-      FooView.render("index.html", %{name: "world"})
+      MyAppWeb.FooView.render("index.html", %{name: "world"})
       |> Phoenix.HTML.Safe.to_iodata()
       |> IO.iodata_to_binary()
 
     assert result == "Hello world!\n"
+  end
+
+  test "supports nested views" do
+    result =
+      MyAppWeb.Nested.FooView.render("index.html", %{name: "world"})
+      |> Phoenix.HTML.Safe.to_iodata()
+      |> IO.iodata_to_binary()
+
+    assert result == "Nested hello world!\n"
   end
 end
