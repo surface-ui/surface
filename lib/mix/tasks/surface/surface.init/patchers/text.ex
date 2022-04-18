21f11c0ee1f27edb555d2d73985f5877f31ded91
Josh Price <josh@alembic.com.au>
2022-04-18 21:57:29 +1000

    Fix spelling of variables in surface.init mix task (#590)

--- lib/mix/tasks/surface/surface.init/patchers/text.ex
+++ lib/mix/tasks/surface/surface.init/patchers/text.ex
@@ -1,10 +1,10 @@
 defmodule Mix.Tasks.Surface.Init.Patchers.Text do
   @moduledoc false
 
-  def append_line(code, text, already_pached_text) do
-    already_pached_regex = Regex.compile!("^#{Regex.escape(already_pached_text)}$", "m")
+  def append_line(code, text, already_patched_text) do
+    already_patched_regex = Regex.compile!("^#{Regex.escape(already_patched_text)}$", "m")
 
-    if Regex.match?(already_pached_regex, code) do
+    if Regex.match?(already_patched_regex, code) do
       {:already_patched, code}
     else
       {:patched, String.trim_trailing(code) <> "\n\n#{text}"}
