d8641b775fb0ee9cc4b028d0adb03e1dd4c8ca6b
Tiago Moraes <tiagoefmoraes@gmail.com>
2022-05-09 08:56:44 -0300

    remove deprecated :phoenix compiler

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Surface.MixProject do
       elixir: "~> 1.11",
       description: "A component based library for Phoenix LiveView",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
