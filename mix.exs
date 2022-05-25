6c3eaa8d08cd0de0705a9baae7916d025f9d499a
Marlus Saraiva <marlus.saraiva@gmail.com>
2022-05-24 21:46:42 -0300

    Drop support for Elixir <= v1.12

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Surface.MixProject do
     [
       app: :surface,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       description: "A component based library for Phoenix LiveView",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
