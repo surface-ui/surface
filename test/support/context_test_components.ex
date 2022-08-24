5e9c8bd7245ba79a33cda3af4729705dd1b911ea
Marlus Saraiva <marlus.saraiva@gmail.com>
2022-08-24 15:17:42 -0300

    Introduce render_sface/1 to allow using Context.* in external templates

--- test/support/context_test_components.ex
+++ test/support/context_test_components.ex
@@ -1,5 +1,12 @@
 defmodule Surface.ContextTest.Components do
   defmodule ComponentWithExternalTemplateUsingContext do
     use Surface.Component
+
+    alias Surface.Components.ContextTest.Outer
+
+    def render(assigns) do
+      assigns = Context.copy_assign(assigns, {Outer, :field})
+      render_sface(assigns)
+    end
   end
 end
