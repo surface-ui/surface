0972997656cf488de349bb81e2e6dfa9884e8e47
harmon25 <dougwright1@gmail.com>
2023-10-11 18:14:45 -0400

    updates to liveview 0.20.x - runs mix deps.update --all

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Surface.MixProject do
   defp deps do
     [
       {:jason, "~> 1.0"},
-      {:phoenix_live_view, "~> 0.19.0"},
+      {:phoenix_live_view, "~> 0.20.0"},
       {:floki, "~> 0.25.0", only: :test},
       {:phoenix_ecto, "~> 4.0", only: :test},
       {:sourceror, "~> 0.12.0"},
