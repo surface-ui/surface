81a9c579ed90eebe319551a6662f157330d4a8f7
Tiago Moraes <tiagoefmoraes@gmail.com>
2024-01-05 08:45:11 -0300

    Fix Card test in generated project (#726)

--- priv/templates/surface.init/demo/card_test.exs
+++ priv/templates/surface.init/demo/card_test.exs
@@ -2,5 +2,5 @@ defmodule <%= inspect(web_module) %>.Components.CardTest do
   use <%= inspect(web_module) %>.ConnCase, async: true
   use Surface.LiveViewTest
 
-  catalogue_test <%= inspect(web_module) %>.Card
+  catalogue_test <%= inspect(web_module) %>.Components.Card
 end
