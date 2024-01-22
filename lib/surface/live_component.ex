c07d65a46a724feb35ecac60442751ca644ce4f3
harmon25 <dougwright1@gmail.com>
2024-01-22 11:35:08 -0500

    mix format

--- lib/surface/live_component.ex
+++ lib/surface/live_component.ex
@@ -63,7 +63,6 @@ defmodule Surface.LiveComponent do
       use Surface.API, include: [:prop, :slot, :data]
       import PhoenixHTMLHelpers
 
-
       @before_compile {Surface.BaseComponent, :__before_compile_init_slots__}
 
       alias Surface.Components.{Context, Raw}
