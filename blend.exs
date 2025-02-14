db46b4c1a29a06fdfe11afcf0dab0963d0c90101
Marlus Saraiva <marlus.saraiva@gmail.com>
2025-02-14 12:01:35 -0300

    Update CI config

--- blend.exs
+++ blend.exs
@@ -1,3 +1,4 @@
 %{
-  phoenix_live_view_0_19: [{:phoenix_live_view, "~> 0.19.0"}]
+  phoenix_live_view_0_19: [{:phoenix_live_view, "~> 0.19.0"}],
+  phoenix_live_view_0_20: [{:phoenix_live_view, "~> 0.20.10"}]
 }
